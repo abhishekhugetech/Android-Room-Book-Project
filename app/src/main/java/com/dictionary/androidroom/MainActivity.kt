9a2ea4a8e4c5bab3c966f0833de6e1f05f87b060
Abhishek <hritik6058@gmail.com>
2019-04-08 00:46:06 +0530

    Added a Fab for hiding and showing the Book Adder

--- app/src/main/java/com/dictionary/androidroom/MainActivity.kt
+++ app/src/main/java/com/dictionary/androidroom/MainActivity.kt
@@ -3,12 +3,15 @@ package com.dictionary.androidroom
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.widget.Toast
 import com.dictionary.androidroom.adapter.BookListAdapter
 import com.dictionary.androidroom.database.Book
@@ -21,6 +24,7 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var adapter:BookListAdapter
     lateinit var mainActivityViewModel: MainActivityViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -56,5 +60,23 @@ class MainActivity : AppCompatActivity() {
         authorName.text.clear()
         bookImage.text.clear()
         bookPrice.text.clear()
+    }
+
+    fun hideAway(view: View){
+        if (adder.visibility == GONE){
+            adder.visibility = VISIBLE
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                floatingActionButton.setImageDrawable(getResources().getDrawable(R.drawable.ic_close_black_24dp));
+            } else {
+                floatingActionButton.setImageDrawable(getResources().getDrawable(R.drawable.ic_close_black_24dp));
+            }
+        }else{
+            adder.visibility = GONE
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                floatingActionButton.setImageDrawable(getResources().getDrawable(R.drawable.ic_add_black_24dp));
+            } else {
+                floatingActionButton.setImageDrawable(getResources().getDrawable(R.drawable.ic_add_black_24dp));
+            }
+        }
     }
 }
